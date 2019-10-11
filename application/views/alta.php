9df55f8afde323bc68bec7d3021b16a0b5f6e6c1
Alberto <alberto.co.morales@gmail.com>
2019-10-10 22:15:02 -0500

    añadidos al formulario alta

--- application/views/alta.php
+++ application/views/alta.php
@@ -1,41 +1,58 @@
 
  <!-- Page -->
  <div class="page">
-      <div class="page-content">
-        <div class="page-content container-fluid">
-        <div class="row">
-                <div class="col-md-12">
+      <div class="page-content">                         
+            
+            <div class="col-lg-12">
             <div class="row">
-            <div class="col-md-3">
-            <div class="panel">
-                <div class="panel-heading">
-                <h3 class="panel-title">Informacion usuario</h3>
-                </div>
-                <div class="panel-body container-fluid">
-                <div class="col-12 col-md-4 bg-white shadow mb-4">
-              <div class="row mt-4 mb-4">
-                <div class="col text-center">
-                  <img class="detail-img img-round" src="https://api.fnkr.net/testimg/500x500/00CED1/FFF/?text=PHOTO" alt="Generic placeholder image">
-                  <h5 class="mt-3 mb-1">Guider</h5>
-                  <h5 class="mb-0"><strong>JING LIN</strong></h5>
-                </div>
-              </div>
-            </div>
-            </div>
-            </div>
-            </div>
-            </div>     
-               
-            <div class="row">
-                <div class="col-md-3">
-
+                <div class="col-md-3">                                
                 <!-- Panel Static Labels -->
-                <div class="panel">
+                <div class="panel panel-info">
+                
                             <div class="panel-heading">
                             <h3 class="panel-title">Informacion usuario</h3>
-                            </div>
+                            </div>                            
                             <div class="panel-body container-fluid">
                             <form autocomplete="off">
+                            <br>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputText">Email</label>
+                                <input type="email" class="form-control" id="emailUser" name="emailUser" placeholder=""
+                                />
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputEmail">Password</label>
+                                <input type="password" class="form-control" id="passUser" name="passUser" placeholder=""
+                                />
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputPassword">Repeat Password</label>
+                                <input type="password" class="form-control" id="RpassUser" name="RpassUser"
+                                    placeholder="" />
+                                </div>                                
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="select">Tipo de usuario</label>
+                                <select class="form-control" id="typeUser" name="typeUser">
+                                    <option selected disabled>Seleccione</option>
+                                    <option>Medico</option>
+                                    <option>Recepcionista</option>
+                                    <option>Cobranza</option>
+                                </select>
+                                </div>                                                              
+                            </form>
+                            </div>
+                        </div>
+                        </div>                    
+                        <!-- End Panel Static Labels -->
+                        <!-- Panel Static Labels -->
+                        <div class="col-md-5">                        
+                        <div class="panel panel-success">                        
+                            <div class="panel-heading">
+                            <h3 class="panel-title">Informacion Personal</h3>
+                            </div>                            
+                            <div class="panel-body container-fluid">
+                            <form autocomplete="off">
+                            <br>
                                 <div class="form-group form-material" data-plugin="formMaterial">
                                 <label class="form-control-label" for="inputText">Text</label>
                                 <input type="text" class="form-control" id="inputText" name="inputText" placeholder="Text"
@@ -152,9 +169,137 @@
                             </form>
                             </div>
                         </div>
-                        <!-- End Panel Static Labels -->
-
-                </div>
+                    </div>
+                    <!-- End Panel Static Labels --> 
+                     <!-- Panel Static Labels -->
+                     <div class="col-md-4">                        
+                        <div class="panel panel-warning">
+                            <div class="panel-heading">
+                            <h3 class="panel-title">Domicilio usuario</h3>
+                            </div>
+                            <div class="panel-body container-fluid">
+                            <form autocomplete="off">
+                            <br>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputText">Text</label>
+                                <input type="text" class="form-control" id="inputText" name="inputText" placeholder="Text"
+                                />
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputEmail">Email</label>
+                                <input type="email" class="form-control" id="inputEmail" name="inputEmail" placeholder="Email"
+                                />
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputPassword">Password</label>
+                                <input type="password" class="form-control" id="inputPassword" name="inputPassword"
+                                    placeholder="Password" />
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputFile">File</label>
+                                <input type="text" class="form-control" placeholder="Browse.." readonly="" />
+                                <input type="file" id="inputFile" name="inputFile" multiple="" />
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="textarea">Textarea</label>
+                                <textarea class="form-control" id="textarea" name="textarea" rows="3"></textarea>
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="select">Select</label>
+                                <select class="form-control" id="select">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="selectMulti">Multi Select</label>
+                                <select class="form-control" id="selectMulti" multiple="">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputDisabled">Input Disabled</label>
+                                <input type="text" class="form-control" id="inputDisabled" placeholder="Input Disabled"
+                                    disabled>
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputFocus">Input Focus</label>
+                                <input type="text" class="form-control focus" id="inputFocus" placeholder="Input Focus">
+                                </div>
+                                <div class="form-group form-material has-warning" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputWarning">Input Warning</label>
+                                <input type="text" class="form-control" id="inputWarning" placeholder="Input Warning">
+                                </div>
+                                <div class="form-group form-material has-danger" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputError">Input Danger</label>
+                                <input type="text" class="form-control" id="inputError" placeholder="Input Error">
+                                </div>
+                                <div class="form-group form-material has-success" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputSuccess">Input Success</label>
+                                <input type="text" class="form-control" id="inputSuccess" placeholder="Input Success">
+                                </div>
+                                <div class="form-group form-material has-info" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputInfo">Input Info</label>
+                                <input type="text" class="form-control" id="inputInfo" placeholder="Input Info">
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputSmall">Small Input</label>
+                                <input type="text" class="form-control form-control-sm" id="inputSmall" name="inputSmall"
+                                    placeholder="Small Input" />
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputSmall">Default Input</label>
+                                <input type="text" class="form-control" id="inputDefault" name="inputDefault" placeholder="Default Input"
+                                />
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputSmall">Large Input</label>
+                                <input type="text" class="form-control form-control-lg" id="inputLarge" name="inputLarge"
+                                    placeholder="Large Input" />
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputHint">Input Hint</label>
+                                <input type="text" class="form-control" id="inputHint" name="inputHint" placeholder="Input Hint"
+                                    data-hint="Write here something cool" />
+                                </div>
+                                <div class="form-group form-material" data-plugin="formMaterial">
+                                <label class="form-control-label" for="inputAddons">Input addons</label>
+                                <div class="input-group">
+                                    <span class="input-group-addon">$</span>
+                                    <div class="form-control-wrap">
+                                    <input type="text" class="form-control" id="inputAddons" />
+                                    </div>
+                                    <span class="input-group-btn">
+                                    <button class="btn btn-outline btn-default" type="button">Button</button>
+                                    </span>
+                                </div>
+                                </div>
+                                <div class="form-group form-material row" data-plugin="formMaterial">
+                                <div class="col-md-6">
+                                    <label class="form-control-label" for="inputGrid1">Input Grid</label>
+                                    <input type="text" class="form-control" id="inputGrid1" name="inputGrid1" placeholder="col-md-6"
+                                    />
+                                </div>
+                                <div class="col-md-6">
+                                    <label class="form-control-label" for="inputGrid2">Input Grid</label>
+                                    <input type="text" class="form-control" id="inputGrid2" name="inputGrid2" placeholder="col-md-6"
+                                    />
+                                </div>
+                                </div>
+                            </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Panel Static Labels --> 
+                </div>       
+                                                
             </div>
             </div>
             </div>
