d392ac86194a350c73236df361207f7d3f1d3b71
BetoG <alberto.co.morales@gmail.com>
2019-10-18 03:51:42 -0500

    homer

---
+++ application/views/homeR.php
@@ -0,0 +1,167 @@
+    <!-- Page -->
+    <div class="page">
+      <div class="page-content">
+        <!-- Panel --> 
+        
+        <div class="col-md-12">
+        <div class="row">
+         <div class="col-md-8">                                                   
+          <div class="col-md-6">
+            <!-- Card -->
+            <div class="card card-block p-20 bg-green-600">
+              <div class="card-watermark darker font-size-80 m-15"><i class="icon wb-clipboard" aria-hidden="true"></i></div>
+              <div class="counter counter-md counter-inverse text-left">
+                <div class="counter-number-group">
+                  <span class="counter-number">25</span>
+                  <span class="counter-number-related text-capitalize">Appointments</span>
+                </div>
+                <div class="counter-label text-capitalize">in book</div>
+              </div>
+            </div>
+            <!-- End Card -->
+          </div>
+
+          <div class="col-md-6">
+            <!-- Card -->
+            <div class="card card-block p-20 bg-blue-600">
+              <div class="card-watermark darker font-size-80 m-15"><i class="icon wb-users" aria-hidden="true"></i></div>
+              <div class="counter counter-md counter-inverse text-left">
+                <div class="counter-number-group">
+                  <span class="counter-number">12</span>
+                  <span class="counter-number-related text-capitalize">Patients</span>
+                </div>
+                <div class="counter-label text-capitalize">in room</div>
+              </div>
+            </div>
+            <!-- End Card -->
+          </div>
+        </div>
+          
+          <div class="col-md-4 masonry-item">
+           
+            <!-- Panel To Do List -->
+            <div class="panel pb-20" id="todo-list">
+              <div class="panel-heading">
+                <h3 class="panel-title">To Do List</h3>
+                <div class="panel-actions panel-actions-keep">
+                  <div class="dropdown">
+                    <a class="panel-action" id="examplePanelDropdown" data-toggle="dropdown" href="#"
+                      aria-expanded="false" role="button"><i class="icon wb-more-vertical" aria-hidden="true"></i></a>
+                    <div class="dropdown-menu dropdown-menu-bullet dropdown-menu-right" aria-labelledby="examplePanelDropdown"
+                      role="menu">
+                      <a class="dropdown-item" href="javascript:void(0)" role="menuitem"><i class="icon wb-flag" aria-hidden="true"></i> Action</a>
+                      <a class="dropdown-item" href="javascript:void(0)" role="menuitem"><i class="icon wb-print" aria-hidden="true"></i> Another action</a>
+                      <a class="dropdown-item" href="javascript:void(0)" role="menuitem"><i class="icon wb-heart" aria-hidden="true"></i> Something else here</a>
+                      <a class="dropdown-item" href="javascript:void(0)" role="menuitem"><i class="icon wb-share" aria-hidden="true"></i> Separated link</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <ul class="list-task list-group h-250" data-plugin="scrollable">
+                <div data-role="container">
+                  <div data-role="content">
+                    <li class="list-group-item task-done" data-plugin="tasklist">
+                      <div class="checkbox-custom checkbox-primary">
+                        <input type="checkbox" id="inputSchedule" name="inputCheckboxesSchedule" checked>
+                        <label for="inputSchedule">
+                          <i class="icon wb-calendar" aria-hidden="true"></i>
+                          <span>Schedule meeting</span>
+                        </label>
+                      </div>
+                    </li>
+                    <li class="list-group-item" data-plugin="tasklist">
+                      <div class="checkbox-custom checkbox-primary">
+                        <input type="checkbox" id="inputCall" name="inputCheckboxesCall">
+                        <label for="inputCall">
+                          <i class="icon wb-bell" aria-hidden="true"></i>
+                          <span>Call clients for follow-up</span>
+                          <span class="badge badge-danger">Today</span>
+                        </label>
+                      </div>
+                    </li>
+                    <li class="list-group-item" data-plugin="tasklist">
+                      <div class="checkbox-custom checkbox-primary">
+                        <input type="checkbox" id="inputBook" name="inputCheckboxesBook">
+                        <label for="inputBook">
+                          <i class="icon wb-calendar" aria-hidden="true"></i>
+                          <span>Book flight for holiday</span>
+                        </label>
+                      </div>
+                    </li>
+                    <li class="list-group-item" data-plugin="tasklist">
+                      <div class="checkbox-custom checkbox-primary">
+                        <input type="checkbox" id="inputForward" name="inputCheckboxesForward">
+                        <label for="inputForward">
+                          <span>Forward important tasks</span>
+                          <span class="badge badge-primary">2 weeks</span>
+                        </label>
+                      </div>
+                    </li>
+                    <li class="list-group-item" data-plugin="tasklist">
+                      <div class="checkbox-custom checkbox-primary">
+                        <input type="checkbox" id="inputRecieve" name="inputCheckboxesRecieve">
+                        <label for="inputRecieve">
+                          <span>Recieve shipment</span>
+                        </label>
+                      </div>
+                    </li>
+                    <li class="list-group-item task-done" data-plugin="tasklist">
+                      <div class="checkbox-custom checkbox-primary">
+                        <input type="checkbox" id="inputSchedule" name="inputCheckboxesSchedule" checked>
+                        <label for="inputSchedule">
+                          <i class="icon wb-calendar" aria-hidden="true"></i>
+                          <span>Schedule meeting</span>
+                        </label>
+                      </div>
+                    </li>
+                    <li class="list-group-item" data-plugin="tasklist">
+                      <div class="checkbox-custom checkbox-primary">
+                        <input type="checkbox" id="inputCall" name="inputCheckboxesCall">
+                        <label for="inputCall">
+                          <i class="icon wb-bell" aria-hidden="true"></i>
+                          <span>Call clients for follow-up</span>
+                          <span class="badge badge-danger">Today</span>
+                        </label>
+                      </div>
+                    </li>
+                    <li class="list-group-item" data-plugin="tasklist">
+                      <div class="checkbox-custom checkbox-primary">
+                        <input type="checkbox" id="inputBook" name="inputCheckboxesBook">
+                        <label for="inputBook">
+                          <i class="icon wb-calendar" aria-hidden="true"></i>
+                          <span>Book flight for holiday</span>
+                        </label>
+                      </div>
+                    </li>
+                    <li class="list-group-item" data-plugin="tasklist">
+                      <div class="checkbox-custom checkbox-primary">
+                        <input type="checkbox" id="inputForward" name="inputCheckboxesForward">
+                        <label for="inputForward">
+                          <span>Forward important tasks</span>
+                          <span class="badge badge-primary">2 weeks</span>
+                        </label>
+                      </div>
+                    </li>
+                    <li class="list-group-item" data-plugin="tasklist">
+                      <div class="checkbox-custom checkbox-primary">
+                        <input type="checkbox" id="inputRecieve" name="inputCheckboxesRecieve">
+                        <label for="inputRecieve">
+                          <span>Recieve shipment</span>
+                        </label>
+                      </div>
+                    </li>
+                  </div>
+                </div>
+              </ul>
+            </div>
+            <!-- End Panel To Do List -->
+
+        </div>
+        
+
+
+    </div>     
+    </div>
+    </div>
+</div>
+    <!-- End Page -->
